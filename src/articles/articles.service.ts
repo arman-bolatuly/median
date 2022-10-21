603ea90323b71ae37b3d4435e969fe158e82ce8a
Arman Bulatuly <arman_bulatovich@mail.ru>
2022-10-21 14:12:49 +0600

    first commit

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  // findDrafts() {
+  //   throw new Error('Method not implemented.');
+  // }
+  constructor(private prisma: PrismaService) {}
+  
+  // CRUD operations
+  create(createArticleDto: CreateArticleDto) {
+    return 'This action adds a new article';
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: false } });
+  }
+
+  findAll() {
+    // return `This action returns all articles`;
+    return this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} article`;
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return `This action updates a #${id} article`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} article`;
+  }
+}
